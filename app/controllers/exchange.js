f0037e494e8d4f26131d26d2aaff3563aba516a9
w0218584 <w0218584@nscc.ca>
2017-05-17 15:56:41 -0300

    created an array with names and duplicted with shuffle

--- app/controllers/exchange.js
+++ app/controllers/exchange.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
+const { get, set, computed } = Ember;
+
 export default Ember.Controller.extend({
+
+  exchangeGenerated: false,
+
   actions: {
         deletePerson: function(id){
             var self = this;
@@ -11,6 +16,43 @@ export default Ember.Controller.extend({
 
                 self.transitionToRoute('exchange.new');
             });
+        },
+
+        generateExchange: function(){
+            let participants = this.get('model').toArray();
+            let senders = [];
+            let receivers = [];
+            let match=true;
+
+            participants.map(function(model){
+                senders.pushObject(model.get('name'));
+                receivers.pushObject(model.get('name'));
+            });
+
+            while(match){
+                match=false;
+                receivers.sort(function(a, b){return 0.5 - Math.random()});
+
+                jQuery.each(senders, function(i, value){		
+                    var iReceiver = jQuery.inArray(receivers[i], senders);
+                    var checkSender = jQuery.inArray(receivers[iReceiver], senders);
+                    if  (checkSender === i){
+                        match=true;
+                        return;
+                    }
+                });
+	        }
+
+            // this.toggleProperty('exchangeGenerated');
         }
-    }
+
+    },
+    exchangResults: computed('exchangeGenerated',function () {
+       
+
+        return get(this, 'exchangeGenerated') ?
+            'This is what is displayed when proper results are generated' :
+            'Click Generate Exchange to see who will be giving a beer to who.';
+
+  })
 });
--- app/models/person.js
+++ app/models/person.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    receiver: DS.attr('string'),
     created: DS.attr('string', {
         defaultValue: function(){
             return new Date();
